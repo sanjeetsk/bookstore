2a84f7d9b001210de5fd7cfbd3d7f1d4d51f2fff
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-28 11:24:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-// api key = AIzaSyBB0_F4CGRPlkVkqwibSTzfyR2wX3b84UM
 
 import React,{useEffect, useState} from "react";
 import axios from "axios";
 import Navbar from "./Component/Navbar/Navbar";
 import Books from "./Component/BookData/Books"
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 const App = () =>{
   // const [books, setBooks] = useState([]);
   const [bookData, setBookData] = useState([]);
 
+
   useEffect(() => {
-    fetchBooks();
+    fetchBooks('harry');
   }, []);
 
   const fetchBooks = async (query) => {
-      axios.get('https://www.googleapis.com/books/v1/volumes?q='+query+'&key=AIzaSyBB0_F4CGRPlkVkqwibSTzfyR2wX3b84UM')
-      .then((res) => setBookData(res.data.items))
-      .catch((err) => console.log(err))
-  }
+    axios.get('https://www.googleapis.com/books/v1/volumes?q='+query+'&key='+API_KEY)
+    .then((res) => setBookData(res.data.items))
+    .catch((err) => console.log(err))
+}
 
   return(
       <div>
